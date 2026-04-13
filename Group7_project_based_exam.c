a21a1f76fddf90fb9862cc8130afc463f2aeda77
honore-nduwayo <nduwayohonore@gmail.com>
2026-04-13 22:50:52 +0000

    Everthing good

--- Group7_project_based_exam.c
+++ Group7_project_based_exam.c
@@ -11,6 +11,7 @@
 // Task 3: applying a discount and checking the entry: Jean Michel
 
 #include <stdio.h>
+#include <string.h>
 ////////////////////TASK 1: SETTING UP THE BOOKING SYSTEM by Honoré //////////////////////////////////
 
 // Setting the limits for passengers and bookings
@@ -18,55 +19,32 @@
 #define MAX_BOOKINGS 100// same as above.
 #define NUM_CODES 5 // This because Each stage has a maximum number of 5 codes
 
-// The follwing are structures for price entries, passengers, and bookings
-//I you don't know baout structures, I recommend reading about them but basically, 
-// they are a way to group different types of data together. 
-//they are like arrays but they can hold different types of data.
-// they are useful for organizing data and making it easier to work with.
-//For example, a Passenger structure can have an account number and a name, 
-//while a Booking structure can have a booking number, account number, start time, stage codes, and total price.
+// Stage 1: Home -> Start Station
+char  stage1_codes[NUM_CODES][3]  = {"C1", "C2", "C3", "C4", "C5"};
+float stage1_prices[NUM_CODES]    = {1.50, 3.00, 4.50, 6.00, 8.00};
 
-struct PriceEntry {
-    char  code[3];
-    float price;
-};
+// Stage 2: Start Station -> End Station
+char  stage2_codes[NUM_CODES][3]  = {"M1", "M2", "M3", "M4", "M5"};
+float stage2_prices[NUM_CODES]    = {5.75, 12.50, 22.25, 34.50, 45.00};
 
-struct Passenger {
-    int  account_number;
-    char name[50];
-};
-
-struct Booking {
-    int   booking_number;
-    int   account_number;
-    char  start_time[6];    // the format is "HH:MM"
-    char  stage1_code[3];
-    char  stage2_code[3];
-    char  stage3_code[3];
-    float total_price;
-};
-
-//PRICE TABLES (pre-loaded)
-
-struct PriceEntry stage1[NUM_CODES] = {
-    {"C1", 1.50}, {"C2", 3.00}, {"C3", 4.50}, {"C4", 6.00}, {"C5", 8.00}
-};
-
-struct PriceEntry stage2[NUM_CODES] = {
-    {"M1", 5.75}, {"M2", 12.50}, {"M3", 22.25}, {"M4", 34.50}, {"M5", 45.00}
-};
-
-struct PriceEntry stage3[NUM_CODES] = {
-    {"F1", 1.50}, {"F2", 3.00}, {"F3", 4.50}, {"F4", 6.00}, {"F5", 8.00}
-};
+// Stage 3: End Station -> Destination
+char  stage3_codes[NUM_CODES][3]  = {"F1", "F2", "F3", "F4", "F5"};
+float stage3_prices[NUM_CODES]    = {1.50, 3.00, 4.50, 6.00, 8.00};
 
 // Arrays to store passengers and bookings, along with counters to keep track of how many we have
 
-struct Passenger passengers[MAX_PASSENGERS];
-int passenger_count = 0;
+int  passenger_account_numbers[MAX_PASSENGERS];
+char passenger_names[MAX_PASSENGERS][50];
+int  passenger_count = 0;
 
-struct Booking bookings[MAX_BOOKINGS];
-int booking_count = 0;
+int   booking_numbers[MAX_BOOKINGS];
+int   booking_account_numbers[MAX_BOOKINGS];
+char  booking_start_times[MAX_BOOKINGS][6];
+char  booking_stage1_codes[MAX_BOOKINGS][3];
+char  booking_stage2_codes[MAX_BOOKINGS][3];
+char  booking_stage3_codes[MAX_BOOKINGS][3];
+float booking_total_prices[MAX_BOOKINGS];
+int   booking_count = 0;
 
 // Function to keep displaying the menu 
 
@@ -79,9 +57,141 @@ void showMenu() {
     printf("Enter your choice: ");
 }
 
-/// Our main function will be the entry point of our program, where we will display
-// the menu and handle user input to perform the desired actions based on the user's choice.
-//and handling user input will be added in Task 2 by Yaa and Jean Michel.
+////////////////////TASK 2: USING THE BOOKING SYSTEM by Yaa//////////////////////////////////
+
+void openAccount() {
+    //to check if there is space in the arrays or bus
+    if(passenger_count >= MAX_PASSENGERS) {
+        printf("Error: The bus is full. Cannot open a new account.\n");
+        return;
+    }
+
+    printf("\n -- Opening a new account for a passenger --\n");
+    printf("Enter passengers name: ");
+
+    getchar();// to consume the newline character left by previous input
+
+    fgets(passenger_names[passenger_count], 50, stdin);
+    passenger_names[passenger_count][strcspn(passenger_names[passenger_count], "\n")] = '\0'; // Remove newline character
+
+    int newID = 1001 + passenger_count; // Generate a new ID based on the current count
+    passenger_account_numbers[passenger_count] = newID;
+
+    printf("Account created! %s, your account number is: %d\n", passenger_names[passenger_count], newID);
+
+    passenger_count++; // Increment the passenger count after adding a new account
+
+}
+
+//Function for booking a journey
+void makeBooking() {
+    int accNum, foundIndex = -1;
+    char s1[3], s2[3], s3[3];
+    float total = 0.0;
+
+    printf("\n--- Book a New Journey ---\n");
+    printf("Enter your account number: ");
+    scanf("%d", &accNum);
+
+    // checking if the number exists
+    for (int i = 0; i < passenger_count; i++) {
+        if (passenger_account_numbers[i] == accNum)
+        {
+            foundIndex = i;
+            break;
+        }
+        
+    }
+
+    if (foundIndex == -1) {
+        printf("Error: Account number %d not found. Please try again.\n", accNum);
+        return;
+    }
+
+    //Journey details
+    printf("Enter your start time (hr:min): ");
+    scanf("%s", booking_start_times[booking_count]);
+
+    printf("Enter stage 1 code(C1-C5): ");
+    scanf("%s", s1);
+
+    printf("Enter stage 2 code(M1-M5): ");
+    scanf("%s", s2);
+
+    printf("Enter stage 3 code(F1-F5): ");
+    scanf("%s", s3);
+
+    // Calculating the bus fare using strcmp
+    for (int i = 0; i < NUM_CODES; i++) {
+
+        if(strcmp(s1, stage1_codes[i]) == 0) total += stage1_prices[i];
+        if(strcmp(s2, stage2_codes[i]) == 0) total += stage2_prices[i];
+        if(strcmp(s3, stage3_codes[i]) == 0) total += stage3_prices[i];
+    }
+
+    // Store the booking details into arrays
+    booking_account_numbers[booking_count] = accNum;
+
+    strcpy(booking_stage1_codes[booking_count], s1);
+    strcpy(booking_stage2_codes[booking_count], s2);
+    strcpy(booking_stage3_codes[booking_count], s3);
+
+    booking_total_prices[booking_count] = total;
+
+    //Generate a unique booking number or ID
+    booking_numbers[booking_count] = 5001 + booking_count;
+
+    printf("\nBooking successfully saved!! your ID: %d | Total Price: $%.2f\n", 
+        booking_numbers[booking_count], total);
+
+    booking_count++;
+}
+
+////////////////////TASK 3: APPLYING DISCOUNT & CONFIRMING by Jean Michel//////////////////////////////////
+
+void applyDiscountAndConfirm()
+{
+    int last = booking_count - 1;
+
+    if (last < 0)
+    {
+        printf("No booking available.\n");
+        return;
+    }
+
+    int hour;
+    sscanf(booking_start_times[last], "%d", &hour);
+
+    // Discount
+    if (hour < 10)
+    {
+        booking_total_prices[last] *= 0.6;
+        printf("\n40%% discount applied!\n");
+    }
+
+    // Display
+    printf("\n--- Booking Details ---\n");
+    printf("Booking ID: %d\n", booking_numbers[last]);
+    printf("Account Number: %d\n", booking_account_numbers[last]);
+    printf("Start Time: %s\n", booking_start_times[last]);
+    printf("Total Price: $%.2f\n", booking_total_prices[last]);
+
+    // Confirm
+    char choice;
+    printf("Confirm booking? (Y/N): ");
+    scanf(" %c", &choice);
+
+    if (choice == 'Y' || choice == 'y')
+    {
+        printf("Booking confirmed!\n");
+    }
+    else
+    {
+        printf("Booking cancelled.\n");
+        booking_count--;
+    }
+}
+
 
 int main() {
     int choice;
@@ -92,12 +202,11 @@ int main() {
 
         switch (choice) {
             case 1:
-                // Task 2 - Yaa: add openAccount() here
-                printf("Coming in Task 2.\n");
+                openAccount();
                 break;
             case 2:
-                // Task 2 & 3 - Yaa / Jean Michel: add makeBooking() her
-                printf("Coming in Task 2 & 3.\n");
+                makeBooking();
+                applyDiscountAndConfirm();
                 break;
             case 0:
                 printf("Goodbye!\n");
